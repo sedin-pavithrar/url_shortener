611e52cdaa9f80ceb16084ebd7133d58c8d489ea
sedin-pavithrar <pavithra.r@sedintechnologies.com>
2026-07-07 22:38:02 +0530

    generated short code and saved it to db

--- urlshortener/shortener/forms.py
+++ urlshortener/shortener/forms.py
@@ -1,22 +1,7 @@
-import string 
-import secrets 
-from .models import ShortURL
+from django import forms 
 
-def generate_code(length = 6 ):
-    """
-    Generates a random 6 character alphanumeric string 
-
-    """
-    characters = string.ascii_letters + string.digits 
-    return ''.join(secrets.choice(characters) for _ in range(length ))
-
-def unique_code():
-    """
-    Generates a unique code until by checking db  
-
-    """
-    while True:
-        code = generate_code()
-        if not ShortURL.objects.filter(short_code = code).exists():
-            return code 
-    
+class GetUrl(forms.Form):
+    original_url = forms.URLField(
+        label = "Original URL ",
+        max_length= 500 
+    )
--- urlshortener/shortener/urls.py
+++ urlshortener/shortener/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .import views 
 
-urlpatters = [
-    path("",views.home,name = "home")
+urlpatterns = [
+    path("",views.home_view,name = "home")
 ]
 
