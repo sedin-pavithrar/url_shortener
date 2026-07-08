5683f4eea7c64f1be01a5805e544a590a128d0e5
sedin-pavithrar <pavithra.r@sedintechnologies.com>
2026-07-08 14:59:42 +0530

    Fix manage.py path in build.sh

--- urlshortener/build.sh
+++ urlshortener/build.sh
@@ -4,6 +4,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python urlshortener/manage.py collectstatic --no-input
 
-python manage.py migrate
+python urlshortener/manage.py migrate
