e5b7c9da896c6b13ebd702d3df224cbc00ee5b69
sedin-pavithrar <pavithra.r@sedintechnologies.com>
2026-07-08 14:35:51 +0530

    Added build.sh for deployment

--- urlshortener/urlshortener/settings.py
+++ urlshortener/urlshortener/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = "django-insecure-)t=p1_m2iaonvvc=y7$9yb=n*dwrtjr@9e$h7q_fin2^r+*nic
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".onrender.com",
+]
 
 
 # Application definition
@@ -43,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -117,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
